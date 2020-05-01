40c2dd3732b781d01e127d0528d408e6509ee065
Kevin <38255521+Snowball115@users.noreply.github.com>
2020-05-01 04:13:39 +0100

    asdf

--- StrategyGameUtilityAI/Assets/Scripts/DebugPanel.cs
+++ StrategyGameUtilityAI/Assets/Scripts/DebugPanel.cs
@@ -8,22 +8,37 @@ using UnityEngine.UI;
 /// </summary>
 public class DebugPanel : MonoBehaviour
 {
-    public GameObject agentToObserve;
-    public GameObject agentNameField;
-    public GameObject agentActionNameField;
+    // Fields in editor
+    public GameObject AgentToObserve;
+    public GameObject NameField;
+    public GameObject ActionNameField;
+    public GameObject ScoreField;
 
+    // Pre caching text fields
+    private Text nameFieldText;
+    private Text actionNameFieldText;
+    private Text scoreFieldText;
+
+    // The Utility Agents data
     private UtilityAgent agentData;
 
     void Start()
     {
-        agentData = agentToObserve.GetComponent<UtilityAgent>();
+        nameFieldText = NameField.GetComponent<Text>();
+        actionNameFieldText = ActionNameField.GetComponent<Text>();
+        scoreFieldText = ScoreField.GetComponent<Text>();
+
+        agentData = AgentToObserve.GetComponent<UtilityAgent>();
     }
 
     void Update()
     {
-        agentNameField.GetComponent<Text>().text = agentData.gameObject.name;
+        nameFieldText.text = agentData.gameObject.name;
 
         if (agentData.currentAction != null)
-            agentActionNameField.GetComponent<Text>().text = agentData.currentAction.ToString();
+        {
+            actionNameFieldText.text = agentData.currentAction.ToString();
+            scoreFieldText.text = agentData.currentAction.UtilityScore.ToString();
+        }
     }
 }
