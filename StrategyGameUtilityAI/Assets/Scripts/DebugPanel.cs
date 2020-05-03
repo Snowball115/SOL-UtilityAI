c193df4d6f0332dd0163e6dbae4e8bad386555aa
Kevin <38255521+Snowball115@users.noreply.github.com>
2020-05-03 05:04:37 +0100

    Update DebugPanel.cs

--- StrategyGameUtilityAI/Assets/Scripts/DebugPanel.cs
+++ StrategyGameUtilityAI/Assets/Scripts/DebugPanel.cs
@@ -39,7 +39,8 @@ public class DebugPanel : MonoBehaviour
         if (agentData.CurrentAction != null)
         {
             actionNameFieldText.text = agentData.CurrentAction.ToString();
-            scoreFieldText.text = decimal.Round((decimal)agentData.CurrentAction.UtilityScore, 3).ToString();
+            //scoreFieldText.text = decimal.Round((decimal)agentData.CurrentAction.UtilityScore, 3).ToString();
+            scoreFieldText.text = agentData.CurrentAction.UtilityScore.ToString("0.000");
         }
     }
 }
